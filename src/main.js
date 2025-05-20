a0fa1b90e2f62f76bcd1881e6ff9e90015dc262f
Sakana <98775292+SakanaDev@users.noreply.github.com>
2025-05-20 10:35:26 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,5 +4,22 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 /* ---------- TO DO ---------- */
+import { createRouter, createWebHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+import Weather from "./components/Weather.vue";
+import Map from "./components/Map.vue";
+import About from './components/About.vue';
+
+const routes = [
+    { path: '/', component: Weather},
+    { path: '/weather', component: Weather},
+    { path: '/map', component: Map},
+    { path: '/about', component: About},
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+createApp(App).use(router).mount('#app')
